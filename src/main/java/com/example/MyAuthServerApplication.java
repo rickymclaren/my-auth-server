142c966619846c63c7fb2ead377d5563e89bba85
Ricky McLaren <ricky.mclaren@cybg.com>
2017-03-26 16:21:03 +0100

    Use JDBC instead of in memory

--- src/main/java/com/example/MyAuthServerApplication.java
+++ src/main/java/com/example/MyAuthServerApplication.java
@@ -4,10 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
@@ -15,18 +19,35 @@ import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableAuthorizationServer
-public class MyAuthServerApplication { //extends AuthorizationServerConfigurerAdapter {
+public class MyAuthServerApplication extends AuthorizationServerConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyAuthServerApplication.class, args);
 	}
 
 	@Autowired
-	DataSource dataSource;
+	private DataSource dataSource;
 
 	@Bean
-	TokenStore tokenStore(DataSource dataSource) {
+	public TokenStore tokenStore() {
 		return new JdbcTokenStore(dataSource);
 	}
 
+	@Bean
+	public AuthorizationCodeServices authorizationCodeServices() {
+		return new JdbcAuthorizationCodeServices(dataSource);
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints
+				.tokenStore(tokenStore())
+				.authorizationCodeServices(authorizationCodeServices());
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients
+				.jdbc(dataSource);
+	}
 }
